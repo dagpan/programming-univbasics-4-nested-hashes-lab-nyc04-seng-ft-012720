b8240be17e055e70ab447ac03baa34fc4d8eee12
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-18 23:21:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,17 +109,23 @@ def adding_to_dennis
 	programmer_hash = 
  		{
         :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
+               :known_for => "COBOL",
+               :languages => ["COBOL", "FORTRAN"]
+                         },
         :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
+               :known_for => "Object Orientation",
+               :languages => ["Smalltalk", "LISP"]
+                     },
         :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-
+               :known_for => "Unix",
+               :languages => ["C"]
+                           }
+ 		}
+ 	programmer_hash[:dennis_ritchie][:languages][0]	
+ 	programmer_hash	
+ 		
 end
+
+
+
+
